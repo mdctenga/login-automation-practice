de76b54f17495006b8e96038c024476e7d484003
mdctenga <mdctenga@gmail.com>
2025-11-18 07:21:57 -1000

    Use page objects as reusable components across test cases

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect, Page } from '@playwright/test';
+import { LoginPage } from '../models/loginPage';
+import { LoggedInPage } from '../models/loggedInPage';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://example.com');
-  expect(await page.title()).toBe('Example Domain');
+test('Positive test login', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const loggedInPage = new LoggedInPage(page);
+  await loginPage.navigate();
+  await loginPage.login('student', 'Password123');
+
+  expect (page.url()).toContain('practicetestautomation.com/logged-in-successfully/');
+  expect (await loggedInPage.isSuccessMessageVisible()).toBeTruthy();
+  expect (await loggedInPage.isLogoutButtonVisible()).toBeTruthy();
+
 });
