658af6524870fa90ceb2c5a30b09b87de60ba08f
mdctenga <mdctenga@gmail.com>
2025-11-18 06:41:27 -1000

    initial commit with a working test

---
+++ tests/login.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://example.com');
+  expect(await page.title()).toBe('Example Domain');
+});
